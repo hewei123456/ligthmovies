d987bd208644f020190e777844507893a4073637
hewei <hwjf123456@sina.cn>
2018-11-26 19:23:28 +0800

    2018-11-26

--- pages/main/main.js
+++ pages/main/main.js
@@ -18,18 +18,34 @@ Page({
     });
   },
 
+  // 显示弹出框
+  showDialog() {
+    this.dialog.showDialog();
+  },
+  //取消事件
+  _cancelEvent(data) {
+    console.log('你点击了取消');
+    this.dialog.hideDialog();
+  },
+  //确认事件
+  _confirmEvent() {
+    console.log('你点击了确定');
+    this.dialog.hideDialog();
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-
+    //获得dialog组件
+    this.dialog = this.selectComponent("#dialog");
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    
   },
 
   /**
